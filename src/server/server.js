bccec3e33e7c68837576e0c23e852a5a0d685812
PundakPundak <57396133+PundakPundak@users.noreply.github.com>
2019-12-15 16:20:19 +0200

    dist and node_modules path fix

--- src/server/server.js
+++ src/server/server.js
@@ -2,12 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const api = require('../server/routes/api')
+const path = require('path')
 // const request = require(`request`)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use( '/', api )
-
+//Dist and node_modules
+app.use(express.static(path.join(__dirname, '..', 'dist')))
+app.use(express.static(path.join(__dirname, '..', 'node_modules')))
+//
 // Mongoose setup
 // const mongoose = require('mongoose')
 // const City = require('./server/model/City')
